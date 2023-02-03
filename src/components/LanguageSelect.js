7378fed0a12adb7de685447845471123c13656e4
Harry <harry.huang887@gmail.com>
2023-02-03 04:11:10 -0500

    updated event handler

--- src/components/LanguageSelect.js
+++ src/components/LanguageSelect.js
@@ -1,15 +1,76 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 
 export default function SelectLabels() {
     const [language, setLanguage] = useState('English');
 
-    const handleChange = (event: SelectChangeEvent) => {
-        setAge(event.target.value);
+    const handleChange = (event) => {
+        setLanguage(event.target.value);
+
+        // When you pick english
+        if (event.target.value === "English") {
+            const englishElements = document.querySelectorAll(".English");
+            englishElements.forEach((element) => {
+                element.classList.remove("hide");
+                element.classList.add("show");
+            });
+
+            const koreanElements = document.querySelectorAll(".Korean");
+            koreanElements.forEach((element) => {
+                element.classList.remove("show");
+                element.classList.add("hide");
+            });
+
+            const chineseElements = document.querySelectorAll(".Chinese");
+            koreanElements.forEach((element) => {
+                element.classList.remove("show");
+                element.classList.add("hide");
+            });
+        } 
+        // When you pick korean
+        else if(event.target.value === "Korean"){
+            const englishElements = document.querySelectorAll(".English");
+            englishElements.forEach((element) => {
+                element.classList.remove("show");
+                element.classList.add("hide");
+            });
+
+            const koreanElements = document.querySelectorAll(".Korean");
+            koreanElements.forEach((element) => {
+                element.classList.remove("hide");
+                element.classList.add("show");
+            });
+
+            const chineseElements = document.querySelectorAll(".Chinese");
+            koreanElements.forEach((element) => {
+                element.classList.remove("show");
+                element.classList.add("hide");
+            });
+        }
+        // When you pick chinese
+        else if(event.target.value === "Chinese"){
+            const englishElements = document.querySelectorAll(".English");
+            englishElements.forEach((element) => {
+                element.classList.remove("show");
+                element.classList.add("hide");
+            });
+
+            const koreanElements = document.querySelectorAll(".Korean");
+            koreanElements.forEach((element) => {
+                element.classList.remove("show");
+                element.classList.add("hide");
+            });
+
+            const chineseElements = document.querySelectorAll(".Chinese");
+            koreanElements.forEach((element) => {
+                element.classList.remove("hide");
+                element.classList.add("show");
+            });
+        }
     };
 
     return (

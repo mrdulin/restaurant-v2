a898e9631ca143770e16248e7c29539f7c925c18
Harry <harry.huang887@gmail.com>
2023-02-04 05:43:08 -0500

    language select works

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux'
+import { currentLanguage } from './LanguageSlice';
 import Card from '@mui/material/Card'
 import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent'
@@ -6,61 +8,72 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box'
 
 const Items = (props) => {
-    return (
-        <Card className='item'>
-                <CardMedia
-                    className='foodPic'
-                    image={props.image}
-                    title={props.name}
-                />
+  const [itemsLanguage, setItemsLanguage] = useState('English');
+  const storeLanguage = useSelector(currentLanguage);
 
-                <div>
-                    <CardContent className='show english'>
-                        <Typography
-                            gutterBottom
-                            variant="h5"
-                            component="div"
-                            className="english"
-                        >
-                            {props.engName}
-                        </Typography>
-                        <Typography
-                            gutterBottom
-                            variant="h8"
-                            component="div"
-                            className="english"
-                        >
-                            {props.engDescription}
-                        </Typography>
-                    </CardContent>
+  useEffect(() => {
+    setItemsLanguage(storeLanguage);
+  }, [storeLanguage]);
 
-                    <CardContent className='hide korean'>
-                        <Typography
-                            gutterBottom
-                            variant="h5"
-                            component="div"
-                            className="korean"
-                        >
-                            {props.korName}
-                        </Typography>
-                        <Typography
-                            gutterBottom
-                            variant="h4"
-                            component="div"
-                            className="korean"
-                        >
-                            {props.korDescription}
-                        </Typography>
-                    </CardContent>
+  return (
+    <Card className='item'>
+      <CardMedia
+        className='foodPic'
+        image={props.image}
+        title={props.name}
+      />
 
-                    <CardContent>
-                        <Typography variant="body2" color="text.secondary">
-                            ${props.price}
-                        </Typography>
-                    </CardContent>
-                </div>
-        </Card>
-    )
-}
+      <div>
+        {itemsLanguage === 'English' && (
+          <CardContent className='english'>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="english"
+            >
+              {props.engName}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h8"
+              component="div"
+              className="english"
+            >
+              {props.engDescription}
+            </Typography>
+          </CardContent>
+        )}
+
+        {itemsLanguage === 'Korean' && (
+          <CardContent className='korean'>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="korean"
+            >
+              {props.korName}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h4"
+              component="div"
+              className="korean"
+            >
+              {props.korDescription}
+            </Typography>
+          </CardContent>
+        )}
+
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            ${props.price}
+          </Typography>
+        </CardContent>
+      </div>
+    </Card>
+  );
+};
 
 export default Items;

5c261525740153d1500fc5c63f2fca8212b0e1c0
mrdulin <novaline.dulin@gmail.com>
2023-07-27 13:54:58 +0800

    fix: image 404 after deploying to github page

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js'
+    path: __dirname + "/public",
+    filename: "[name].[contenthash].js",
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
